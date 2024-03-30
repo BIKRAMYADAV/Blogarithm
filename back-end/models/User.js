74123bd9a83e35f8daad166e9aa8f20a8957edcc
BIKRAMYADAV <bikram.deep.yadav.11@gmail.com>
2024-03-30 23:57:17 +0530

    added code that creates a jwt

---
+++ back-end/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    firstname : {
+        type : String,
+        required : true
+    },
+    lastname : {
+        type : String
+    }
+    ,
+    email : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    token : {
+        type : String,
+        default : null
+    }
+})
+
+module.exports = mongoose.model('user', UserSchema)
--- back-end/server.js
+++ back-end/server.js
@@ -4,10 +4,14 @@ const Blog = require('./models/Blog')
 const PORT = 3000
 const cors = require('cors')
 const app = express()
-
+const User = require('./models/User')
+const bcrypt =  require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cookieparser = require('cookie-parser')
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(cookieparser())
 
 connectDB()   
  
@@ -29,5 +33,87 @@ app.post('/addBlog', async (req, res) => {
     res.send(result);
    } catch (error) {
     res.status(500).send(error.message);
+   }
+})
+
+app.post('/register', async (req, res) => {
+   try{
+      const {firstname, lastname, email, password} = req.body
+      if(!(firstname && lastname && email && password)) {
+            res.status(400).send('All field are compulsory')
+      }
+      const existinguser = await User.findOne({email})
+      if(existinguser){
+         res.status(400).send('user already exists')
+      }
+      //encrypting
+      const myencpassword = bcrypt.hash(password,10)
+
+      const user = await User.create({
+         firstname,
+         lastname,
+         email,
+         password: myencpassword
+      })
+
+      //generating a token for user and sending it
+     const token =  jwt.sign(
+         {id: user._id, email: email},
+         'shhh',
+         {
+            expiresIn : "2h"
+         }
+      );
+      user.token = token
+      user.password = undefined
+
+      res.status(201).json(user)
+
+   }
+   catch(error){
+         console.log(error);
+   }
+})
+
+app.post('/login', async (req, res) => {
+   try{
+      //get all data from frontend
+      const {email, password} = req.body
+      //validation
+      if (!(email && password)) {
+         res.status(400).send('send all fields')
+      }
+
+      //find user in db
+      const user = await User.findOne({email})
+      //match the password
+      
+      if(user && (await bcrypt.compare(password, user.password))){
+       const token =   jwt.sign(
+            {id : user._id},
+            'shhh',
+            {
+               expiresIn : "2h"
+            }
+         );
+         user.token = token
+         user.password = undefined
+
+         //cookie section
+         const options = {
+            expires : new Date(Date.now() + 3 *24 *60*60*1000),
+            httpOnly : true
+         }
+         res.status(200).cookie("token", token, options).json({
+            success : true,
+            token,
+            user
+         })
+      }
+
+      //send a token
+   }
+   catch (error) {
+      console.log(error);
    }
 })
